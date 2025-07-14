8e6859c31c2280e84889510a448c5dedb79f5b6b
Kok Jia Yin <jiayinkok@gmail.com>
2025-07-14 20:03:19 +0800

    Force Dark Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
+          forcedTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
